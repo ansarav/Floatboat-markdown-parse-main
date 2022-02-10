b408669cc8326b844d5a3f55a3f12e2604203785
ansarav <ansaravi@ucsd.edu>
2022-02-09 18:30:34 -0800

    w

---
+++ lib/demoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class demoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.add(1,3);// purposefully make it fail
+        // that way you can pull and in github 
+
+    }
+}
+
+public static int Substract( int a, int b){
+    return a - b;
+}
