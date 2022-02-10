09bcf591ceec9ac08942844881dc550ab00b2594
ansarav <ansaravi@ucsd.edu>
2022-02-09 19:34:32 -0800

    a

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,42 +1,4 @@
-/* import static org.junit.Assert.*;
-import org.junit.*;
-import java.util.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
-    public void multiplication() {
-        assertEquals(10, 5*2);
-    }
-    
-   
-    /* public void testLink()  throws IOException{
-        String[] links = {"https://something.com","some-page.html"};
-        List<String> Array4 = Arrays.asList("http:…");
-        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(regLines));
-    } */
-
-
-    /* & Start Not this*/  
-    /* 
-    @Test
-    public void testLink() throws IOException {
-        String regFile = Files.readString(Path.of("./test-file.md"));
-        String[] regLines = regFile.split("\n");
-        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(regLines));
-    } */
-
-   
-/* 
- } */ 
 
  import static org.junit.Assert.*;
  import org.junit.*;
